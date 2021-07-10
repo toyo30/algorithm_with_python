de68c1a54c35fc44f944319cd1b9bd341916b06e
toyo30 <55613446+toyo30@users.noreply.github.com>
2021-07-10 16:46:16 +0900

    Add files via upload

--- 코드잇 알고리즘3.2-12 퀵 정렬 patition 구현.py
+++ 코드잇 알고리즘3.2-12 퀵 정렬 patition 구현.py
@@ -1,12 +1,55 @@
 # 두 요소의 위치를 바꿔주는 helper function
 def swap_elements(my_list, index1, index2):
+    p = index2
+    i = index1
+    b = index1
+    while i <= p:
+        if my_list[i] > my_list[p]:
+            i +=1
+        elif my_list[i] <= my_list[p]:
+            temp = my_list[b]
+            my_list[b] = my_list[i]
+            my_list[i] = temp
+            # my_list[b], my_list[i] = my_list[i], my_list[b]
+            i +=1
+            b +=1
+    return my_list
     # 코드를 작성하세요.
-
-
-# 퀵 정렬에서 사용되는 partition 함수
 def partition(my_list, start, end):
-    # 코드를 작성하세요.
+    c = my_list[end]
+    pivot_list = swap_elements(my_list, start, end)
+    return pivot_list.index(c)
 
+
+###   답안코드
+# # 두 요소의 위치를 바꿔주는 helper function
+# def swap_elements(my_list, index1, index2):
+#     temp = my_list[index1]
+#     my_list[index1] = my_list[index2]
+#     my_list[index2] = temp
+
+
+# # 퀵 정렬에서 사용되는 partition 함수
+# def partition(my_list, start, end):
+#     # 리스트 값 확인과 기준점 이하 값들의 위치 확인을 위한 변수 정의
+#     i = start
+#     b = start
+#     p = end
+
+#     # 범위안의 모든 값들을 볼 때까지 반복문을 돌린다
+#     while i < p:
+#         # i 인덱스의 값이 기준점보다 작으면 i와 b 인덱스에 있는 값들을 교환하고 b를 1 증가 시킨다
+#         if my_list[i] <= my_list[p]:
+#             swap_elements(my_list, i, b)
+#             b += 1
+#         i += 1
+
+#     # b와 기준점인 p 인덱스에 있는 값들을 바꿔준다
+#     swap_elements(my_list, b, p)
+#     p = b
+
+#     # pivot의 최종 인덱스를 리턴해 준다
+#     return p
 
 # 테스트 1
 list1 = [4, 3, 6, 2, 7, 1, 5]
