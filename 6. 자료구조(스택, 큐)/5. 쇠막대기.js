c0b680565df07ffa7df6e432b87166dbe72b3127
Minky Baek <55613446+toyo30@users.noreply.github.com>
2021-10-08 23:43:58 +0900

    Add files via upload

--- 6. 자료구조(스택, 큐)/5. 쇠막대기.js
+++ 6. 자료구조(스택, 큐)/5. 쇠막대기.js
@@ -1,14 +1,39 @@
+// let ttt = '()(((()()8)9(10())()))(())';
 function solution(str) {
     var answer = 0;
-    var stack = [];
-    for(let x of str){
-        if(x === '(') stack.push()
 
-        else if(x === ')') stack.pop()
+    for(let x = 0; x < 10; x++){
+        let stack = [];
+        let pointNum = 0;
+        for(let t = x; t < str.length; t++){
+
+            if(str[t] === '(') {
+                stack.push(str[t]);
+            } 
+            
+            else if(str[t] === ')'){
+                stack.pop();
+                if(stack.length === 0) {
+                    if(str[t-1] === '(' || str[t-1] == undefined) {
+                        break;
+                    }
+                    else if(){
+                        answer += pointNum + 1;
+                        break;
+                    }
+                } 
+                else {
+                    if(str[t-1] === '('){
+                        pointNum++;
+                    }
+                    
+                }
+            }
+
+        }
+
     
     }
-    
-    answer += 1;
     
     return answer;
 }
