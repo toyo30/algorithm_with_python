89f81712d08f56b7a90c56a1d6ae378e4475cc15
Minky Baek <55613446+toyo30@users.noreply.github.com>
2021-10-10 15:35:14 +0900

    Add files via upload

--- 6. 자료구조(스택, 큐)/6. 공주 구하기.js
+++ 6. 자료구조(스택, 큐)/6. 공주 구하기.js
@@ -1,27 +1,32 @@
+// 스택으로 풀려고 했음. 스택으로도 풀 수 있긴 함. 
 function solution(N, K) {
     var answer = 0;
     let stack = [];
 
     let num = [];
     for(let i = 1; i <= N; i++){
-        num.push(i)
+        num.push(i);
     }
 
     while(true){
+        for(let x = 0; x < num.length; x++){
+            stack.push(num[x])
+
+            if(num.length === 2){
+                stack.push(num[x])
+            }
+
+            if(stack.length === K) {
+
+                stack = [];
+                stack.push(num[x + 1])
+                num.splice(num.indexOf(num[x]), 1)
+            }
+        }
+
         if(num.length === 1) {
             answer = num[0];
             break;  
-        }
-
-        for(let x of num){
-            stack.push(x)
-            
-            if(stack.length === K) {
-                
-                let a = stack.pop()
-                num.splice(num.indexOf(a), 1)
-                continue;
-            }
         }           
     }
     return answer;
